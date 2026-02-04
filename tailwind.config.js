78a819d7a27441c85e2ddbd274c040dcb8e13e39
Andrés López Corrales <andreslopezcorrales326@gmail.com>
2026-02-04 00:58:32 -0700

    styles(tailwind): add tailwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Lavender: "#DCE4F9",
+        Amarillo: "	#f8db27"
+      }
+    },
+  },
+  plugins: [],
+}
